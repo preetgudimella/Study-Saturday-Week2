c3aae12c9d8a44c22b033ddb67932034df0d4972
Preet Gudimella <preet.gudimella@gmail.com>
2018-11-18 11:08:46 -0500

    Study Saturday Week 2

--- db/models/test.js
+++ db/models/test.js
@@ -23,3 +23,42 @@ const Test = db.define('test', {
 Test.belongsTo(Student, { as: 'student' });
 
 module.exports = Test;
+
+
+
+
+
+
+
+
+
+const Sequelize = require('sequelize');
+const db = require('../db');
+const Student = require('./student');
+
+const Test = db.define('test', {             // Creating the models (models are tables represented as objects)
+  subject: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  grade: {
+    type: Sequelize.FLOAT,
+    allowNull: false
+  }
+})
+
+// const Test = db.define('test', {
+//   subject: {
+//     type: Sequelize.STRING,
+//     allowNull; false
+//   },
+//   grade: {
+//     type: Sequelize.FLOAT,
+//     allowNull; false
+//   }
+// })
+
+Test.belongsTo(Student)
+Student.hasMany(Test);
+
+module.exports = Test;
