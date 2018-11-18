b7fffcbe45b3c665718f3ade444bb1eb8f37aad5
Preet Gudimella <preet.gudimella@gmail.com>
2018-11-18 11:07:45 -0500

    Study Saturday Week 2

--- db/models/student.js
+++ db/models/student.js
@@ -36,3 +36,49 @@ Student.beforeCreate(student => {
 });
 
 module.exports = Student;
+
+
+
+
+
+
+
+
+
+
+
+
+'use strict';
+
+const Sequelize = require('sequelize');
+const db = require('../db');                        // ???
+
+const Student = db.define('student', {             // Creating the models (models are tables represented as objects)
+  
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true
+    }
+  }
+})
+
+// ??? how to know which hook to choose based on spec
+// http://docs.sequelizejs.com/manual/tutorial/hooks.html#declaring-hooks
+Student.beforeCreate(student => {                          // Options isn't needed in: Student.beforeCreate((user, options) => {
+  const firstName = student.firstName;
+  const lastName = student.lastName;
+  student.firstName = firstName[0].toUpperCase() + firstName.slice(1);
+  student.lastName = lastName[0].toUpperCase() + lastName.slice(1);
+})
+
+module.exports = Student;                           // Exporting
